28ea225cb67a6e5fd9c9a7e6d2bc7a4b2398e449
RohanKumarSachdeva <rohan231094@gmail.com>
2021-05-13 15:08:28 +0530

    created game surface that quits on pressing cross

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
+import pygame, sys
 
+# Initializing pygame
+pygame.init()
+# Game surface
+screen_width = 1280
+screen_height = 900
+game_screen = pygame.display.set_mode((screen_width, screen_height))
+
+while True:
+    # Start of every iteration we check for any event
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+            # Draw all elements
+    pygame.display.update()
