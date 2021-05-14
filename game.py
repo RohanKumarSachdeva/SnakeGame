41f2a759e1c24260cb3782a11345b09db8573894
RohanKumarSachdeva <rohan231094@gmail.com>
2021-05-14 17:42:57 +0530

    Added fruit class that creates and draws fruit class objects

--- game.py
+++ game.py
@@ -1,16 +1,37 @@
 import pygame
+from pygame.math import Vector2
 import sys
+import random
+
+
+# Fruit Class
+class Fruit:
+    def __init__(self):
+        # X and Y position of Fruit
+        self.x = random.randint(0, cell_count - 1)
+        self.y = random.randint(0, cell_count - 1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        # Create a rectangle surface using x,y coordinate, width and height.
+        fruit_rect = pygame.Rect(self.pos.x * cell_size,
+                                 self.pos.y * cell_size,
+                                 cell_size, cell_size)
+        # Draw the above fruit surface on game_screen
+        pygame.draw.rect(game_screen, (126, 166, 144), fruit_rect)
+
 
 pygame.init()
 # Game display surface
-screen_width = 1280
-screen_height = 900
-game_screen = pygame.display.set_mode((screen_width, screen_height))
+cell_size = 40
+cell_count = 20
+game_screen = pygame.display.set_mode((cell_size * cell_count,
+                                       cell_size * cell_count))
 pygame.display.set_caption('Snake Game')
 # To restrict program's frame rate
 clock = pygame.time.Clock()
-test_surface = pygame.Surface((100, 200))
-test_surface.fill((0, 0, 255))
+# Instantiating fruit object
+fruit = Fruit()
 
 while True:
     # Start of every iteration we check for any event
@@ -20,7 +41,7 @@ while True:
             sys.exit()
 
     game_screen.fill((175, 215, 70))
-    game_screen.blit(test_surface, (200, 250))
+    fruit.draw_fruit()
     # Refreshing game surface
     pygame.display.update()
     # Restricting while loop to run 60 times per second
